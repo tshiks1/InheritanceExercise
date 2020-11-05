c9def03cbc0da45080a4d58ca2af1cbb35417a39
Teresia Ngigi <tshiks22@outlook.com>
2020-11-05 13:36:00 -0600

    break

---
+++ Inheritance/Animal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    class Animal
+    {
+
+        public bool LivesOnLand { get; set; } 
+        public string Name(string name)
+        {
+            return name;
+        }
+        public int Weight
+        {
+            get;set;
+        }
+        public bool CanFly { get; set; } = true;
+    }
+
+}
---
+++ Inheritance/Bird.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    class Bird :Animal
+    {
+        public bool CanFly { get; set; }
+        public void Color()
+        {
+
+        }
+
+
+    }
+}
